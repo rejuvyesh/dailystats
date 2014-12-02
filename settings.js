32aceabf405ea7f62593fa7ad76d318c6a2e8d6f
rejuvyesh <mail@rejuvyesh.com>
2014-12-02 12:58:35 +0530

    browsing

--- settings.js
+++ settings.js
@@ -4,4 +4,4 @@ var display_groups = [];
 display_groups.push(["read-djvu", "read-pdf", "read-epub", "read-mobi", "code-web", "code-python", "code-haskell", "code-ruby", "code-matlab", "write", "term", "mail"]);
 // Unproductive
 display_groups.push(["irc", "social", "communities", "tv-local", "tv-web", "read-rss"]);
-display_groups.push(["misc"]);
+display_groups.push(["misc", "browsing"]);
